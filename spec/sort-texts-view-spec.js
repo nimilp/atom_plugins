0a5e27ee69c56a2a87b0afb81a05b38cf40e0951
Nimil Peethambaran <nimil.peethambaran@iq-ity.com>
2017-08-07 12:47:08 -0400

    initial commit

---
+++ spec/sort-texts-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SortTextsView from '../lib/sort-texts-view';
+
+describe('SortTextsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
